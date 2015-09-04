f366d5be1cb9857e8d41865dadb4877105a34e66
TsT <tst2005@gmail.com>
2015-09-04 14:51:22 +0200

    try to follow the sanbox.lua API

--- lib/isolation.lua
+++ lib/isolation.lua
@@ -49,23 +49,23 @@ local function make_safe_getsetmetatable(unsafe_getmetatable, unsafe_setmetatabl
 	return safe_getmetatable, safe_setmetatable
 end
 
-local function setup_g(g, _G, config)
+local function setup_g(g, master, config)
 	assert(type(g)=="table")
-	assert(type(_G)=="table")
+	assert(type(master)=="table")
 	assert(type(config)=="table")
 
 	for i,k in ipairs{
 		"_VERSION", "assert", "error", "ipairs", "next", "pairs",
 		"pcall", "select", "tonumber", "tostring", "type", "unpack","xpcall",
 	} do
-		g[k]=_G[k]
+		g[k]=master[k]
 	end
 
-	local safe_getmetatable, safe_setmetatable = make_safe_getsetmetatable(_G.getmetatable,_G.setmetatable)
+	local safe_getmetatable, safe_setmetatable = make_safe_getsetmetatable(master.getmetatable,master.setmetatable)
 	g.getmetatable = assert(safe_getmetatable)
 	g.setmetatable = assert(safe_setmetatable)
 	g.print = function() end
-	g._G = g -- self
+	g["_G"] = g -- self
 end
 local function setup_package(package, config)
 	package.config	= require"package".config or "/\n;\n?\n!\n-\n"
@@ -76,7 +76,7 @@ local function setup_package(package, config)
 end
 local function cross_setup_g_package(g, package, config)
 	local loaded = package.loaded
-	loaded._G	= g		-- add _G as loaded modules
+	loaded["_G"]	= g		-- add _G as loaded modules
 	
 	-- global register all modules
 	--for k,v in pairs(loaded) do g[k] = v end
@@ -95,11 +95,13 @@ local function cross_setup_g_package(g, package, config)
 end
 
 
-local function new_env(_G, conf)
-	assert(_G)
+local function new_env(master, conf)
+	assert(master) -- the real _G
 	local config = {}
 	for k,v in pairs(_M.defaultconfig) do config[k]=v end
-	for k,v in pairs(conf) do config[k]=v end
+	if type(conf) == "table" then
+		for k,v in pairs(conf) do config[k]=v end
+	end
 	assert( config.package )
 	assert( config.package_wanted )
 	assert( config.g_content )
@@ -110,7 +112,7 @@ local function new_env(_G, conf)
 	assert(req("package") == package)
 	assert(package.loaded.package == package)
 
-	setup_g(g, _G, config)
+	setup_g(g, master, config)
 	setup_package(package, config)
 	cross_setup_g_package(g, package, config)
 
@@ -119,14 +121,63 @@ local function new_env(_G, conf)
 	return g
 end
 
-local function run(f, env)
-	local ce = require("compat_env")
-	return ce.load(f, nil, nil, newenv)
+local ce_load = require("compat_env").load
+--local function run(f, env)
+--	return ce_load(f, nil, nil, env)
+--end
+
+local funcs = {
+	dostring = function(self, str, ...)
+		return pcall(function(...) return ce_load(str, str, 't', self.env)(...) end, ...)
+	end,
+	run = function(self, str, ...)
+		local function filter(ok, ...)
+			self.lastok = ok
+			if not ok then
+				self.lasterr = ...
+				return nil
+			end
+			self.lasterr = nil
+			return ...
+		end
+		if type(str) == "function" then
+			print("STR = function here", #{...}, ...)
+			return require"compat_env".setfenv(f, self.env)(...)
+			--return filter(pcall(function(...)
+			--	return assert(ce_load(string.dump(str), nil, 'b', self.env))(...)
+			--end))
+		end
+		return filter( pcall( function(...)
+			return assert(ce_load(str, str, 't', self.env))(...)
+		end) )
+	end,
+	dofunction = function(self, func, ...)
+		assert( type(func) == "function")
+		return pcall(function(...) return func(...) end, ...)
+	end,
+	runf = function(self, func, ...)
+		assert( type(func) == "function")
+		local ok, t_ret = pcall(function(...) return {func(...)} end, ...)
+		if ok then
+			return t_ret
+		else
+			return nil
+		end
+	end,
+}
+local new_mt = { __index = funcs, }
+
+local function new(master, conf)
+	local e = new_env(master or _G, conf)
+	local o = setmetatable( {env = e}, new_mt)
+	assert(o.env)
+	return o
 end
 
 local defaultconfig = require "isolation.defaults".defaultconfig
 
-_M.new = new_env
+--_M.new_env = new_env
+_M.new = new
 _M.run = run
 _M.defaultconfig = defaultconfig
 
